cae3c6a606701a0a477139fe9ce5d1a6d459f67b
郭 <jienfu926@gmail.com>
2021-10-23 16:02:52 +0800

    ＊first commit＊

---
+++ dist/precache-manifest.b9ecb6a2252231b2138af46c715eb742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0e1dc4b0bc3090aede19521070e75854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbb1e15ba3902e1f2bd",
+    "url": "/js/app.efca2290.js"
+  },
+  {
+    "revision": "62f5d3718e1bc4b2ec79",
+    "url": "/js/chunk-vendors.86a6e42f.js"
+  },
+  {
+    "revision": "926434255bc94853fde66b028d16576b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
