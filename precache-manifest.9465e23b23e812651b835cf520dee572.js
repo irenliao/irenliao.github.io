7567edff7b868cd8392cdcbc718890bd8bf0864a
郭 <jienfu926@gmail.com>
2021-10-25 15:11:53 +0800

    ＊build 檔案＊

---
+++ precache-manifest.9465e23b23e812651b835cf520dee572.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aab3106d0ef8a4455e",
+    "url": "/css/app.b64d1810.css"
+  },
+  {
+    "revision": "fb8585a1eb34670f01c8c3c85a35daf5",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "6ae7094e3697b3e1bc35496bf139e42f",
+    "url": "/img/1.6ae7094e.jpg"
+  },
+  {
+    "revision": "a5a4c61ba545e6e30fb7290e12821b46",
+    "url": "/img/2.a5a4c61b.jpg"
+  },
+  {
+    "revision": "0b7f7c263d1e1370f798d979a274f110",
+    "url": "/img/3.0b7f7c26.jpg"
+  },
+  {
+    "revision": "066c7ebac99d0b1fa06fb2cd12278ce3",
+    "url": "/img/4.066c7eba.jpg"
+  },
+  {
+    "revision": "1af9e463fc7cd432d39fe51d329b17da",
+    "url": "/img/5.1af9e463.jpg"
+  },
+  {
+    "revision": "fb8585a1eb34670f01c8c3c85a35daf5",
+    "url": "/img/app-logo.fb8585a1.png"
+  },
+  {
+    "revision": "57be9698865266cc744947d38baaf1d6",
+    "url": "/img/homepage.57be9698.png"
+  },
+  {
+    "revision": "69772d6905edf53a8e4a29827431bf3a",
+    "url": "/img/imac.69772d69.png"
+  },
+  {
+    "revision": "453c7c9a77776d17ef41b2299d0fe0a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28aab3106d0ef8a4455e",
+    "url": "/js/app.7990ce9a.js"
+  },
+  {
+    "revision": "c3cd5a6ff61db628ade9",
+    "url": "/js/chunk-vendors.71adc34f.js"
+  },
+  {
+    "revision": "926434255bc94853fde66b028d16576b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0ba5c741ed5256b1c447b2543d2f1942",
+    "url": "/media/video-scenario1.0ba5c741.mp4"
+  },
+  {
+    "revision": "69fb200e6bdf4172e4843207c6e52e5b",
+    "url": "/media/video-scenario2.69fb200e.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
